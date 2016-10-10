cbe23beea2af68fd315da62cae44a1e149330825
Jose Luis Lopez <joseluis.lopez@ohmycode.cat>
2016-10-10 10:41:55 +0200

    Uber-jar working
    pom.xml modified and jar with dependencies working correctly

--- src/main/java/testXdocreport/Application.java
+++ src/main/java/testXdocreport/Application.java
@@ -1,6 +1,7 @@
 package testXdocreport;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,8 +17,10 @@ public class Application {
 	public static void main(String[] args) {
 	    try {
 	      // 1) Load file by filling Velocity template engine and cache it to the registry
-	      InputStream in =Application.class.getResourceAsStream("DocxProjectWithVelocity.docx");
-	      IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in,TemplateEngineKind.Velocity);
+	      InputStream in = Application.class.getResourceAsStream("DocxProjectWithVelocity.docx");
+	    	//InputStream in = new FileInputStream(new File("C:/DocxProjectWithVelocity.docx"));
+	      IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in, TemplateEngineKind.Velocity);
+	      
 
 	      // 2) Create context Java model
 	      IContext context = report.createContext();
@@ -27,6 +30,7 @@ public class Application {
 	      // 3) Generate report
 	      OutputStream out = new FileOutputStream(new File("DocxProjectWithVelocity_out.docx"));
 	      report.process(context, out);
+	      out.close();
 
 	    } catch (IOException e) {
 	      e.printStackTrace();
